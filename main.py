ce823d64433850abd24c8001810865f1c05bd752
kere199 <kereleishviliirakli81@gmail.com>
2023-10-09 06:52:36 +0400

    create snake segments

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-import turtle as t
+import turtle as t 
+from turtle import Screen, Turtle
 
 screen = t.Screen()
 screen.bgcolor("black")
 # screen.screensize(canvwidth=600, canvheight=600, bg="black")
 screen.setup(width=600, height=600)
+
+
+positions = [(0,0), (20,0), (40,0)]
+segments = []
+for pos in positions:
+    new_turtle = Turtle()
+    new_turtle.shape("square")
+    new_turtle.color("white")
+    new_turtle.goto(pos)
+    segments.append(new_turtle)
+
 
 screen.exitonclick()
