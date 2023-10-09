5b304aab6eedf03077d2557281a54fe2274db022
kere199 <kereleishviliirakli81@gmail.com>
2023-10-09 06:37:26 +0400

    screen setup

---
+++ main.py
@@ -0,0 +1,8 @@
+import turtle as t
+
+screen = t.Screen()
+screen.bgcolor("black")
+# screen.screensize(canvwidth=600, canvheight=600, bg="black")
+screen.setup(width=600, height=600)
+
+screen.exitonclick()
